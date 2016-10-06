11fd56ff27ef6abc71ca0fcf53e2a960bdc9ef6c
niallstack <niallstack@outlook.com>
2016-10-06 10:22:32 +0100

    Finished Basic and Reduced Views

--- AdvDatabaseProject/ViewDoc.py
+++ AdvDatabaseProject/ViewDoc.py
@@ -35,14 +35,71 @@ def printCpuByCoresDoc(event):
     for r in cpuByCoresDoc_list:
         listbox.insert(END, r)
 
+def printManByManDoc(event):
+    manByManDoc_list = db.view('_design/manufacturer/_view/by_manufacturer_doc')
+    listbox.delete('0', END)
+    for r in manByManDoc_list:
+        listbox.insert(END, r)
+
+def printManByModPri(event):
+    manByModPri_list = db.view('_design/manufacturer/_view/by_model_price')
+    listbox.delete('0', END)
+    for r in manByModPri_list:
+        listbox.insert(END, r)
+
+def printPriByManMod(event):
+    priByManMod_list = db.view('_design/video_cards/_view/by_manufacturer_model_price')
+    listbox.delete('0', END)
+    for r in priByManMod_list:
+        listbox.insert(END, r)
+
+def printRedByManModStock(event):
+    redByManModStck_list = db.view('_design/reduce/_view/by_manufacturer_model_stock')
+    listbox.delete('0', END)
+    for r in redByManModStck_list:
+        listbox.insert(END, r)
+
+def printRedByManSum(event):
+    redByManSum_list = db.view('_design/reduce/_view/by_manufacturer_sum')
+    listbox.delete('0', END)
+    for r in redByManSum_list:
+        listbox.insert(END, r)
+
+
 basicButton = Button(frame, text="Basic View")
-compByManModButton = Button(frame, text="Components By Manufacturer and Model")
-cpuByCoresDocButton = Button(frame, text="CPU By Cores and Document")
+compByManModButton = Button(frame, text="Components by Manufacturer and Model")
+cpuByCoresDocButton = Button(frame, text="CPU by Cores and Document")
+ManByManDocButton = Button(frame, text="Manufacturer by Manufacturer and Document")
+ManByModPriButton = Button(frame, text="Manufacturer by Model and Price")
+PriByManModButton = Button(frame, text="Price by Manufacturer and Model")
+RedByManModStockButton = Button(frame, text="Reduced View by Manufacturer, Model and Stock")
+RedByManSumButton = Button(frame, text="Reduced View by Manufacturer and Sum")
+
+viewsLabel = Label(frame, text="----Views----")
+viewsLabel.pack()
+
 basicButton.bind("<Button-1>", printBasic)
 compByManModButton.bind("<Button-1>", printCompByManMod)
 cpuByCoresDocButton.bind("<Button-1>", printCpuByCoresDoc)
+ManByManDocButton.bind("<Button-1>", printManByManDoc)
+ManByModPriButton.bind("<Button-1>", printManByModPri)
+PriByManModButton.bind("<Button-1>", printPriByManMod)
+RedByManModStockButton.bind("<Button-1>", printRedByManModStock)
+RedByManSumButton.bind("<Button-1>", printRedByManSum)
+
 basicButton.pack()
 compByManModButton.pack()
 cpuByCoresDocButton.pack()
+ManByManDocButton.pack()
+ManByModPriButton.pack()
+PriByManModButton.pack()
+
+ReducesLabel = Label(frame, text="----Reduces----")
+ReducesLabel.pack()
+
+RedByManModStockButton.pack()
+RedByManSumButton.pack()
+
+
 
 window.mainloop()
